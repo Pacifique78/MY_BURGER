54e42c85ea252d287f740f5df5f9f4f8d122a455
Pacifique78 <tuyipaccy78@gmail.com>
2020-04-11 09:35:39 +0200

    adding routes to my app

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,26 +84,15 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Pacifique',
-                address: {
-                    street: 'KK417',
-                    zipcode: '123456',
-                    country: 'Rwanda'
-                },
-                email: 'kigalicodingacademy@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-        .then((response) => {
-            this.setState({loading: false, purchasing: false});
-        }).catch(error => {
-            this.setState({loading: false, purchasing: false});
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
         });
     }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App appTitle="Person Manager" />, document.getElementById('root'));
+const app = (
+    <BrowserRouter>
+        <App appTitle="Person Manager"/>
+    </BrowserRouter>
+)
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
